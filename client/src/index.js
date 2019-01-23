ce78afc6f7047915746b54a918c43ec358b28b13
yohan fairfort <yohan.fairfort@gmail.com>
2019-01-23 09:30:54 +0100

    Add of Hello world using truffle Getting started

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import drizzle functions and contract artifact
+import { Drizzle, generateStore } from "drizzle";
+import MyStringStore from "./contracts/MyStringStore.json";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+// let drizzle know what contracts we want
+const options = { contracts: [MyStringStore] };
+
+// setup the drizzle store and drizzle
+const drizzleStore = generateStore(options);
+const drizzle = new Drizzle(options, drizzleStore);
+
+// pass in the drizzle instance
+ReactDOM.render(<App drizzle={drizzle} />, document.getElementById("root"));
 serviceWorker.unregister();
